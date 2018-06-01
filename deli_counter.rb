dce860d6de755c85182a8d1af50eacf9e4f8e3b8
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-01 23:32:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0
+    intro = "The line is currently:"
+    katz_deli.each_with_index do |value, index|
+      intro << " #{index + 1}. #{value}"
+    end
+    puts intro
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  inLine = katz_deli << name
+  puts inLine
+end
