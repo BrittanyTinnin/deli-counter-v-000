54bc3834bfee239c5e181853590eeba2d561c6a8
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-02 00:49:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,11 +22,11 @@ end
 
 def now_serving(name)
   binding.pry
-  if katz_deli.length >= 1
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
     # katz_deli << name
+  else
     puts "Currently serving #{name}."
     katz_deli.shift
-  else
-    puts "There is nobody waiting to be served!"
   end
 end
