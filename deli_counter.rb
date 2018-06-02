f9c29c91c77e1ff740fd0122dd403048ab46c083
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-02 00:51:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,13 +20,13 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(name)
+def now_serving(katz_deli)
   binding.pry
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
     # katz_deli << name
   else
-    puts "Currently serving #{name}."
+    puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
   end
 end
